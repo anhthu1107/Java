8466ba74afaa1a70490769915cfa2605bf728a04
Anh Thu <anhthuu1107@gmail.com>
2025-09-15 13:22:48 +0700

    update

--- TuhocJava/src/bai11_ifelse_bt5.java
+++ TuhocJava/src/bai11_ifelse_bt5.java
@@ -12,6 +12,29 @@ ax^2 + bx + c =0
 public class bai11_ifelse_bt5 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        //nhập vào 3 hệ số a, b, c
+        System.out.println("Mời nhập vào hệ số a: ");
+        double a=sc.nextDouble();
+        System.out.println("Mời nhập vào hệ số b: ");
+        double b=sc.nextDouble();
+        System.out.println("Mời nhập vào hệ số c: ");
+        double c=sc.nextDouble();
+
+        //tính denta
+        double delta= Math.pow(b,2) -(4*a*c); //Math.pow là bình phương
+        //kiểm tra điều kiện
+        if (delta <0)
+            System.out.println("Phương trình vô nghiệm.");
+        else if (delta==0){
+            double x= -b/(2*a);
+            System.out.println("pt có nghiệm kép x1 = x2 =" +x);
+        }
+        else {
+            double x1 = (-b+Math.sqrt(delta))/(2*a); //Math.sqrt là căn
+            double x2 = (-b-Math.sqrt(delta))/(2*a);
+            System.out.println("Phương trình có 2 nghiệm phân biệt: ");
+            System.out.println("x1 = " +x1);
+            System.out.println("x2 = " +x2);
+        }
     }
 }
